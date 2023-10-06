86919035f618c13bb951387838d48290250e4bb1
Yigit <yigitcan.ustek@lotec.com.tr>
2023-10-06 14:44:52 +0300

    Update and delete function added.

--- index.js
+++ index.js
@@ -3,31 +3,39 @@ const { engine } = require('express-handlebars');
 const path = require('path');
 const client = require('./db');
 
-// Example query
+
 
 const PORT = 3000;
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', async (req, res) => {
-    const queryString = 'SELECT * FROM todo';
+    const queryString = 'SELECT * FROM todo ORDER BY id ASC';
     const data = (await client.query(queryString)).rows;
 
     res.render('home', {
         message: 'Challenge 2',
         data: data,
-        // id: data.map((item) => item.id),
-        // title: data.map((item) => item.title),
-        // assignee: data.map((item) => item.assignee),
-        // done: data.map((item) => item.done),
+       
     });
 });
 
-app.post('/insert_data', async (req, res) => {
+app.get('/user/:userId', async (req, res) => {
+    const queryString = `SELECT * FROM todo WHERE (id = ${req.params.userId});`;
+    const data = (await client.query(queryString)).rows;
+   
+   
+    res.render('user', {
+        data: data,
+    });
+});
+
+
+app.post('/', async (req, res) => {
     const formData = req.body; // Form data sent from the client
     console.log(formData);
     
@@ -36,7 +44,35 @@ app.post('/insert_data', async (req, res) => {
     await client.query(queryString);
 
     res.redirect('/');
+   
 });
+
+
+app.post('/delete', async (req,res)=>{
+    const queryString = `DELETE FROM todo WHERE (id = ${req.body.silText})`;
+    await client.query(queryString);
+
+
+    res.redirect('/');
+});
+
+
+app.post('/update', async (req,res)=>{
+    const queryString = `UPDATE todo 
+                            SET
+                                id=${req.body.id},
+                                title='${req.body.title}',
+                                assignee='${req.body.assignee}',
+                                done=${req.body.done} 
+                            WHERE (id = ${req.body.oldId})`;
+    client.query(queryString).then(()=>{
+        res.redirect('/');
+    }).catch((err)=>{
+        res.send(err);
+    })
+    
+});
+
 
 app.listen(PORT, () => {
     console.log('Server is listening on port 3000');
